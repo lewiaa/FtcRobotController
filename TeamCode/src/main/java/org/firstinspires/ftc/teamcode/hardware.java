8536b260c976d91a40be5c68dd87cbe620a4f368
lewiaa <47694500+lewiaa@users.noreply.github.com>
2021-01-15 12:09:53 -0500

    driving function made

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.hardware.Sensor;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+public class Hardware4Motor {
+    HardwareMap hwMap = null;
+ 
+ 
+    public Servo flicker = null;
+    public Servo pusher = null;
+    public DcMotor fl = null;
+    public DcMotor fr = null;
+    public DcMotor bl = null;
+    public DcMotor br = null;
+    public DcMotor intake = null;
+    public DcMotor shooter = null;
+    
+    public BNO055IMU imu = null;
+    public Orientation angles = null;
+
+    public void init(HardwareMap ahwMap) {
+        hwMap = ahwMap;
+        detectorInit();
+
+ 
+        shooter = hwMap.get(DcMotor.class, "shooter");
+        fl = hwMap.get(DcMotor.class, "fl");
+        fr = hwMap.get(DcMotor.class, "fr");
+        bl = hwMap.get(DcMotor.class, "bl");
+        br = hwMap.get(DcMotor.class, "br");
+        intake = hwMap.get(DcMotor.class, "intake");
+        flicker = hwMap.get(Servo.class, "flicker")
+        pusher = hwMap.get(Servo.class, "pusher")
+
+        fl.setPower(0);
+        fr.setPower(0);
+        bl.setPower(0);
+        br.setPower(0);
+        intake.setPower(0);
+        shooter.setPower(0);
+
+
+        shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        br.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        imu = hwMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+        bl.setDirection(DcMotor.Direction.REVERSE);
+        br.setDirection(DcMotor.Direction.REVERSE);
+
+
+    }
+
+
+
+
+}
